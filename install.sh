7bf057ef9baf8e3ccf696d08b490d53fa6cc014d
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-01-11 17:02:33 +0900

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
 
+# dotfiles
 ln -sfnv $(pwd)/.vimrc ~/.vimrc
 ln -sfnv $(pwd)/.gitconfig ~/.gitconfig
 ln -sfnv $(pwd)/.ideavimrc ~/.ideavimrc
 ln -sfnv $(pwd)/.tmux.conf ~/.tmux.conf
+ln -sfnv $(pwd)/.zshrc.common ~/.zshrc.common
 
-# foldoer
-ln -sfnv $(pwd)/bin ~/bin
+# bin
+mkdir -p ~/.local/bin
+ln -sfnv $(pwd)/.local/bin/tmx ~/.local/bin/tmx
+
+# if .zshrc exists and the last line does not include .zshrc.common, include it
+if [[ ! -z $(ls ~/.zshrc) && $(cat ~/.zshrc | tail -n 1) != "source ~/.zshrc.common" ]]; then 
+    echo "" >> ~/.zshrc;
+    echo "# Automatically added" >> ~/.zshrc;
+    echo "source ~/.zshrc.common" >> ~/.zshrc; 
+fi 
