0c469b910ac3ba4e0f82491984fefa619e278a42
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-01-12 20:03:05 +0900

    fix

--- install.sh
+++ install.sh
@@ -16,11 +16,11 @@ ln -sfnv $(pwd)/.local/bin/tmx ~/.local/bin/tmx
 #######################################################################################################
 # install oh my zsh. This also changes default shell to zsh
 if [[ ! -e ~/.oh-my-zsh ]]; then 
-    yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-# if [[ -z $(echo ${SHELL} | grep zsh) ]]; then 
-#     chsh -s $(which zsh); 
-# fi
+if [[ -z $(echo ${SHELL} | grep zsh) ]]; then 
+    chsh -s $(which zsh); 
+fi
 touch ~/.zshrc
 # if .zshrc exists and the last line does not include .zshrc.common, include it
 if [[ -z $(cat ~/.zshrc | grep "source ~/.zshrc.common") ]];  then
