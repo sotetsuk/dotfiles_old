df340d6c838f69f10e0fc99715b4c282f1fa1214
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-01-12 20:03:18 +0900

    Install zsh (#6)
    * add fzf to required
    
    * use zsh as default
    
    * install oh my zsh
    
    * fix typo
    
    * fix

--- install.sh
+++ install.sh
@@ -11,8 +11,19 @@ ln -sfnv $(pwd)/.zshrc.common ~/.zshrc.common
 mkdir -p ~/.local/bin
 ln -sfnv $(pwd)/.local/bin/tmx ~/.local/bin/tmx
 
+#######################################################################################################
+# zsh
+#######################################################################################################
+# install oh my zsh. This also changes default shell to zsh
+if [[ ! -e ~/.oh-my-zsh ]]; then 
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+if [[ -z $(echo ${SHELL} | grep zsh) ]]; then 
+    chsh -s $(which zsh); 
+fi
+touch ~/.zshrc
 # if .zshrc exists and the last line does not include .zshrc.common, include it
-if [[ -e ~/.zshrc && $(cat ~/.zshrc | tail -n 1) != "source ~/.zshrc.common" ]]; then 
+if [[ -z $(cat ~/.zshrc | grep "source ~/.zshrc.common") ]];  then
     echo "" >> ~/.zshrc;
     echo "# Automatically added" >> ~/.zshrc;
     echo "source ~/.zshrc.common" >> ~/.zshrc; 
