5a1526384dc0b6c9a1df5fa79367cf5029093004
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-01-12 18:57:57 +0900

    fix: bug in install

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ mkdir -p ~/.local/bin
 ln -sfnv $(pwd)/.local/bin/tmx ~/.local/bin/tmx
 
 # if .zshrc exists and the last line does not include .zshrc.common, include it
-if [[ ! -z $(ls ~/.zshrc) && $(cat ~/.zshrc | tail -n 1) != "source ~/.zshrc.common" ]]; then 
+if [[ -e ~/.zshrc && $(cat ~/.zshrc | tail -n 1) != "source ~/.zshrc.common" ]]; then 
     echo "" >> ~/.zshrc;
     echo "# Automatically added" >> ~/.zshrc;
     echo "source ~/.zshrc.common" >> ~/.zshrc; 
