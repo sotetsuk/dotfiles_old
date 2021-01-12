05f73b39d617494334384c30a95dfe5f2705c662
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-01-13 03:31:04 +0900

    feat: install bat

--- install.sh
+++ install.sh
@@ -43,3 +43,10 @@ if [[ ! -e ~/.fzf ]]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     yes | ~/.fzf/install
 fi
+
+#######################################################################################################
+# bat
+#######################################################################################################
+if [[ -z $(which bat) && -n $(which batcat) ]]; then
+    ln -sfnv $(which batcat) ~/.local/bin/bat
+fi
