1352a1fb98d37767f203f885276a67c1661a86f7
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-01-12 19:41:56 +0900

    use zsh as default

--- install.sh
+++ install.sh
@@ -11,8 +11,13 @@ ln -sfnv $(pwd)/.zshrc.common ~/.zshrc.common
 mkdir -p ~/.local/bin
 ln -sfnv $(pwd)/.local/bin/tmx ~/.local/bin/tmx
 
+# zsh
+if [[ -z $(echo ${SHELL} | grep zsh) ]]; then 
+    chsh -s $(which zsh); 
+fi
+touch ~/.zshrc
 # if .zshrc exists and the last line does not include .zshrc.common, include it
-if [[ -e ~/.zshrc && $(cat ~/.zshrc | tail -n 1) != "source ~/.zshrc.common" ]]; then 
+if [[ -z $(cat ~/.zshrc | grep "source ~/.zshrc.common") ]];  then
     echo "" >> ~/.zshrc;
     echo "# Automatically added" >> ~/.zshrc;
     echo "source ~/.zshrc.common" >> ~/.zshrc; 
