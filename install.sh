e8ad11ad8980ed732e6e6dfb61d5126c90a787d9
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-01-12 23:46:34 +0900

    refactor: how to add symbolic link

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
+#######################################################################################################
 # dotfiles
-ln -sfnv $(pwd)/.vimrc ~/.vimrc
-ln -sfnv $(pwd)/.gitconfig ~/.gitconfig
-ln -sfnv $(pwd)/.ideavimrc ~/.ideavimrc
-ln -sfnv $(pwd)/.tmux.conf ~/.tmux.conf
-ln -sfnv $(pwd)/.zshrc.common ~/.zshrc.common
+#######################################################################################################
+dotfiles="
+.vimrc
+.gitconfig
+.ideavimrc
+.tmux.conf
+.zshrc.common
+.local/bin/tmx
+"
 
-# bin
-mkdir -p ~/.local/bin
-ln -sfnv $(pwd)/.local/bin/tmx ~/.local/bin/tmx
+for dotfile in ${dotfiles}; do
+    ln -sfnv $(pwd)/${dotfile} ~/${dotfile}
+done
 
 #######################################################################################################
 # zsh
