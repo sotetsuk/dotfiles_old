e6c978b99053268cfcac24f418536fe8bc8cb695
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-03-23 08:15:22 +0900

    Revert "debug"
    This reverts commit 3c8a49738fa9691ed72be6cacac31fa85ddaa9ab.

--- install.sh
+++ install.sh
@@ -54,13 +54,9 @@ if [[ ! -e ~/.fzf ]]; then
     ~/.fzf/install --all --key-bindings --completion --update-rc
 fi
 
-echo "fzf install is finished!"
-
 #######################################################################################################
 # bat
 #######################################################################################################
 if [[ -z $(which bat) && -n $(which batcat) ]]; then
     ln -sfnv $(which batcat) ~/.local/bin/bat
 fi
-
-echo "bat install is finished!"
