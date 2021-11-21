ca70942748aa95126d9b7abe0e12a4491ea83ef1
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-11-22 01:21:26 +0900

    fix zsh installer

--- installers/install_zsh.sh
+++ installers/install_zsh.sh
@@ -1,10 +1,16 @@
-# install zsh
-brew install zsh
+#! /bin/bash
+set -eu
 
-# make zsh as default
-if [[ -z $(echo ${SHELL} | grep zsh) ]]; then 
-    chsh -s $(which zsh); 
+if [[ ! $(which zsh) ]]; then
+    # install zsh
+    brew install zsh
+
+    # make zsh as default
+    if [[ -z $(echo ${SHELL} | grep zsh) ]]; then 
+        chsh -s $(which zsh); 
+    fi
 fi
+
 # install oh my zsh.
 if [[ ! -e ~/.oh-my-zsh ]]; then 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
