bbdc96a779ff802360220ebc81d06ca0cdfecb28
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-11-22 05:10:22 +0900

    fix founding cmd

--- installers/install_homebrew.sh
+++ installers/install_homebrew.sh
@@ -8,7 +8,9 @@ if [[ ! -e ~/.zshrc  ]]; then
   exit 1
 fi
 
-if [[ -z $(which brew) ]]; then
+if command -v brew 1>/dev/null 2>&1; then
+  echo "brew found."
+else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   if [[ -e "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
     echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
--- installers/install_zsh.sh
+++ installers/install_zsh.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -eu
 
-if [[ -z $(which zsh) ]]; then
-    # install zsh
-    sudo apt update
-    sudo apt install -y zsh
+if command -v zsh 1>/dev/null 2>&1; then
+  echo "zsh found."
+else
+  # install zsh
+  sudo apt update
+  sudo apt install -y zsh
 fi
 
 # install oh my zsh.
 if [[ ! -e ~/.oh-my-zsh ]]; then 
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 # NOTE: you may need to change the default shell by `chsh -s $(which zsh)`
