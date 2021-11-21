4b865529b0b692c6c2d9e32079f917364346b09e
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-11-22 05:57:38 +0900

    install language server

--- installers/install_vim_lsp.sh
+++ installers/install_vim_lsp.sh
@@ -1,8 +1,8 @@
 vim -c ":PlugInstall" -c "qa!"
 
-# for LANG_SERVER in "pyls-all" "clangd"; do
-#   cd ~/.local/share/vim-lsp-settings/servers
-#   mkdir -p ${LANG_SERVER}
-#   cd ${LANG_SERVER}
-#   ~/.vim/plugged/vim-lsp-settings/installer/install-${LANG_SERVER}.sh
-# done
+for LANG_SERVER in "pyls-all" "clangd"; do
+  cd ~/.local/share/vim-lsp-settings/servers
+  mkdir -p ${LANG_SERVER}
+  cd ${LANG_SERVER}
+  ~/.vim/plugged/vim-lsp-settings/installer/install-${LANG_SERVER}.sh
+done
