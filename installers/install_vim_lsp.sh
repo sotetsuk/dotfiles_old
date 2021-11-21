169861b7dad5b81ffde4c1e87240cc5f89148971
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-11-22 06:22:13 +0900

    fix

--- installers/install_vim_lsp.sh
+++ installers/install_vim_lsp.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+set -eu
+
 vim -c ":PlugInstall" -c "qa!"
 
+mkdir -p ~/.local/share/vim-lsp-settings/servers
 for LANG_SERVER in "pyls-all" "clangd"; do
   cd ~/.local/share/vim-lsp-settings/servers
   if [[ -d ${LANG_SERVER} ]]; then
@@ -8,5 +12,6 @@ for LANG_SERVER in "pyls-all" "clangd"; do
   fi
   mkdir -p ${LANG_SERVER}
   cd ${LANG_SERVER}
-  ~/.vim/plugged/vim-lsp-settings/installer/install-${LANG_SERVER}.sh
+  ~/.vim/plugged/vim-lsp-settings/installer/install-${LANG_SERVER}.sh &
 done
+wait
