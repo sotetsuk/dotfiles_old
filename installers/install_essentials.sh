db8b54837421d17dc852a5ec10bc5170ba223db6
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-11-22 02:31:58 +0900

    fix

--- installers/install_essentials.sh
+++ installers/install_essentials.sh
@@ -4,8 +4,7 @@ set -eu
 # on ubuntu
 OS="$(uname)"
 if [[ "${OS}" == "Linux" ]]; then
-    sudo apt update -y
-    sudo apt update install -y build build-essential git curl
+    sudo apt update && sudo apt update install -y build build-essential git curl
 fi
 
 # ensure essentials
