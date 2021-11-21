ee3e4d16cc024d41403d99d92b3b068c017c264a
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-11-22 00:54:11 +0900

    remove zsh installer from default installer

--- install.sh
+++ install.sh
@@ -37,7 +37,6 @@ brew install \
     wget \
     tree \
     tmux \
-    zsh	\
     bat	\
     xz \
     pre-commit \
@@ -48,14 +47,6 @@ echo "##########################################################################
 echo "# zsh"
 echo "#############################################################################"
 
-# make zsh as default
-if [[ -z $(echo ${SHELL} | grep zsh) ]]; then 
-    chsh -s $(which zsh); 
-fi
-# install oh my zsh.
-if [[ ! -e ~/.oh-my-zsh ]]; then 
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-fi
 # if .zshrc exists and the last line does not include .zshrc.common, include it
 touch ~/.zshrc
 if [[ -z $(cat ~/.zshrc | grep "source ~/.zshrc.common") ]];  then
