a834587d0d252906472dac9b36fe31c430d0784f
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-12-08 17:08:23 +0900

    fix pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -4,5 +4,5 @@ set -eu
 # on ubuntu
 OS="$(uname)"
 if [[ "${OS}" == "Linux" ]]; then
-    sudo apt update -y && sudo apt install -y build-essential git curl
+    sudo apt update -y && sudo apt install -y build-essential git curl python3-venv
 fi
