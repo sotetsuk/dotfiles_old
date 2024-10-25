5c193eae73e94583c609c6abfe4d8ae3f9907294
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-10-26 03:46:15 +0900

    rm install_vim_lsp.sh

--- install.sh
+++ install.sh
@@ -112,4 +112,17 @@ echo "##########################################################################
 echo "# vim-lsp"
 echo "#############################################################################"
 
-./installers/install_vim_lsp.sh
+vim -c ":PlugInstall" -c "qa!"
+
+mkdir -p ~/.local/share/vim-lsp-settings/servers
+for LANG_SERVER in "pylsp-all" "clangd"; do
+  cd ~/.local/share/vim-lsp-settings/servers
+  if [[ -d ${LANG_SERVER} ]]; then
+    echo "passed [${LANG_SERVER}]"
+    continue
+  fi
+  mkdir -p ${LANG_SERVER}
+  cd ${LANG_SERVER}
+  ~/.vim/plugged/vim-lsp-settings/installer/install-${LANG_SERVER}.sh &
+done
+wait
