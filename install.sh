027499303d296418b43fa59f97c53b512c9d9e3a
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2024-10-26 03:51:31 +0900

    pygments

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ fi
 
 # install packages using brew
 # xz is required to install clangd language server in vim-lsp-settings
-brew install tmux gh bat xz xsel tree pygmentize inkscape wget
+brew install tmux gh bat xz xsel tree pygments inkscape wget
 
 echo "#############################################################################"
 echo "# Set dotfiles"
